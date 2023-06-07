515e65cbd299995eebca5797dbc1eb04610699a4
Mees Drissen <mees.drissen@xs4all.nl>
2023-06-07 09:25:11 +0200

    Tweede commit voor flask test.

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World! This is online in Azure now!</p>"
+
+@app.route("/flasktest")
+def flask_test():
+    return "<p>This is a test to see how to commit with continous deployment works!</p>"
